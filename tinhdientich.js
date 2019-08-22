a4aeec2944e7b0c6dec0f7e6235c798145372e46
thao9415 <thao.9415@gmail.com>
2019-08-22 13:43:34 +0700

    file script

---
+++ tinhdientich.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth=prompt("enter the width");
+inputHeight=prompt("enter the Height");
+let width=parseInt(inputWidth);
+let height=parseInt(inputHeight);
+let area=width*height;
+document.write("the area is = "+area);
